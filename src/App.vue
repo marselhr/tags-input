2503e82c38901071638660b925c48fd4852c8c56
marsel945 <rewoherson519@gmail.com>
2024-01-09 17:38:58 +0800

    setup vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import TagInput from './components/TagInput.vue';
+export default {
+  components: {
+    TagInput,
+  },
+};
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <TagInput />
+</template>
+
+<style scoped>
+h1 {
+  color: aqua;
+}
+</style>
