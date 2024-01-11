c93ac729bc41f7e7a3c7af7bd75bc02065a7d8ec
marsel945 <rewoherson519@gmail.com>
2024-01-12 07:54:52 +0800

    fix payload

--- src/http/todo-api.js
+++ src/http/todo-api.js
@@ -7,7 +7,7 @@ const resource = '/todos'
 
 export const allTodos = () => api.get(resource)
 
-export const createTodo = todo => api.post(resource)
+export const createTodo = todo => api.post(resource, todo)
 export const updateTodo = (id, todo) => api.put(`${resource}/${id}`, task)
 export const removeTodo = id => api.delete(`${resource}/${id}`)
 
