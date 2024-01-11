ed827e0f666a0ed7188b63b663310c31347d6529
marsel945 <rewoherson519@gmail.com>
2024-01-11 20:40:09 +0800

    style uncompleted and completed task

---
+++ src/components/todos/Todos.vue
@@ -0,0 +1,11 @@
+<template>
+  <Todo v-for="todo in todos" :todo="todo" :key="todo.id" />
+</template>
+
+<script setup>
+import Todo from './Todo.vue';
+
+defineProps({
+  todos: Array,
+});
+</script>
--- src/pages/TodoPage.vue
+++ src/pages/TodoPage.vue
@@ -1,8 +1,24 @@
 <template>
   <TodoInputVue />
-  <TodoItemVue />
+  <Todos :todos="uncompletedTodo" />
+  <Todos :todos="completedTodo" />
 </template>
 <script setup>
 import TodoInputVue from '../components/TodoInput.vue';
-import TodoItemVue from '../components/TodoItem.vue';
+import Todos from '../components/todos/Todos.vue';
+import { computed, onMounted, ref } from 'vue';
+import { allTodos } from '../http/todo-api';
+const todos = ref([]);
+onMounted(async () => {
+  const { data } = await allTodos();
+
+  todos.value = data.todos;
+});
+
+const uncompletedTodo = computed(() =>
+  todos.value.filter((todo) => !todo.completed)
+);
+const completedTodo = computed(() =>
+  todos.value.filter((todo) => todo.completed)
+);
 </script>
