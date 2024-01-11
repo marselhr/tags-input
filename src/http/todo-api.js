6ce88316aa3eec7a967b8a3cdf5c3d48907243e3
marsel945 <rewoherson519@gmail.com>
2024-01-11 14:24:16 +0800

    setup axios

---
+++ src/http/todo-api.js
@@ -0,0 +1,15 @@
+import api from './api'
+
+
+
+const resource = '/todos'
+
+
+export const allTodos = () => api.get(resource)
+
+export const createTodo = todo => api.post(resource)
+export const updateTodo = (id, todo) => api.put(`${resource}/${id}`, task)
+export const removeTodo = id => api.delete(`${resource}/${id}`)
+
+
+export const completeTodo = (id, todo) => api.put(`${resource}/${id}`, task)
