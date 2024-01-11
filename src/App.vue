95847f3d344280522e8025ed1d7241ad03cbdc3e
marsel945 <rewoherson519@gmail.com>
2024-01-11 14:25:12 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 <template>
   <navbar />
   <main class="min-h-[80vh] w-full flex flex-col items-center">
-    <task-input class="mb-3" />
-    <task-item class="mb-2" />
+    <task-page />
   </main>
+  <footer-item />
 </template>
 
 <style scoped></style>
