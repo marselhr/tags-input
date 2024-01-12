d27d64fbaf50ea45c95dda4bc65ba913fe2deef0
marsel945 <rewoherson519@gmail.com>
2024-01-12 09:01:22 +0800

    style inline input

--- src/components/todos/Todo.vue
+++ src/components/todos/Todo.vue
@@ -1,48 +1,38 @@
 <template>
-  <div
+  <div @dblclick="isEdit = true"
     class="w-4/6 shadow-retro rounded-md border p-1 mb-2 min-h-12 border-slate-700 flex flex-wrap items-center justify-between group transition-all duration-300 ease-in-out"
-    :class="completedClass"
-  >
-    <div class="flex items-center">
-      <input
-        type="checkbox"
-        class="h-4 w-4 border border-green-700 rounded me-2"
-        :checked="todo.completed"
-      />
-      <p class="text-slate-700 break-words max-w-4/6">
+    :class="completedClass">
+    <div class="flex items-center w-full">
+      <input type="checkbox" class="h-4 w-4 border border-green-700 rounded me-2" :checked="todo.completed" />
+
+      <input v-if="isEdit" @keyup.esc="isEdit = false" v-focus type="text"
+        class="flex-grow bg-transparent rounded focus:outline-none" />
+      <span v-else class="text-slate-700 break-words max-w-4/6">
         {{ todo.title }}
-      </p>
+      </span>
     </div>
-    <div class="flex gap-2 transition-all ease-in-out">
-      <!-- <p class="transition delay-500 text-slate-900">{{ todo.updated_at }}</p> -->
-      <div
-        class="gap-2 hidden group-hover:flex transition-all duration-150 ease-in-out float-end"
-      >
-        <button
-          class="flex items-center justify-center text-red-600 outline outline-1 rounded-full w-6 h-6 p-3 hover:bg-red-500 hover:text-slate-100 hover:shadow-retro hover:outline-slate-100 transition-all duration-300 ease-in-out"
-        >
-          <font-awesome-icon :icon="['fas', 'trash']" />
-        </button>
-        <button
-          class="flex justify-center items-center text-green-500 outline outline-1 rounded-full w-6 h-6 p-3 hover:bg-green-600 hover:text-slate-50 hover:shadow-retro hover:outline-slate-50 transition-all duration-300 ease-in-out"
-        >
-          <font-awesome-icon :icon="['fas', 'edit']" />
-        </button>
-      </div>
-    </div>
+    <TodoAction @edit="isEdit = !isEdit" v-show="!isEdit" />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-
+import {computed, ref} from 'vue';
+import TodoAction from './TodoAction.vue';
 const props = defineProps({
   todo: Object,
 });
+
+const isEdit = ref(false);
 
 const completedClass = computed(() =>
   props.todo.completed
     ? 'line-through text-slate-400 bg-gray-200'
     : 'bg-purple-300 text-slate-600 hover:bg-gray-200'
 );
+
+
+
+const vFocus = {
+  mounted: (el) => el.focus()
+}
 </script>
---
+++ src/components/todos/TodoAction.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex gap-2 transition-all ease-in-out">
+        <!-- <p class="transition delay-500 text-slate-900">{{ todo.updated_at }}</p> -->
+        <div class="gap-2 hidden group-hover:flex transition-all duration-150 ease-in-out float-end">
+            <button
+                class="flex items-center justify-center text-red-600 outline outline-1 rounded-full w-6 h-6 p-3 hover:bg-red-500 hover:text-slate-100 hover:shadow-retro hover:outline-slate-100 transition-all duration-300 ease-in-out">
+                <font-awesome-icon :icon="['fas', 'trash']" />
+            </button>
+            <button
+                class="flex justify-center items-center text-green-500 outline outline-1 rounded-full w-6 h-6 p-3 hover:bg-green-600 hover:text-slate-50 hover:shadow-retro hover:outline-slate-50 transition-all duration-300 ease-in-out"
+                @click="emit('edit')">
+                <font-awesome-icon :icon="['fas', 'edit']" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['edit']);
+</script>
